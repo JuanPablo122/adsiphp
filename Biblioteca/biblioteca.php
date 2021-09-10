7cf538ca02bc86153452385a2c2a6c2a7a94bb1d
JuanPablo122 <jpdoncel0@misena.edu.co>
2021-09-10 11:52:19 -0500

    conexion pdo

--- Biblioteca/biblioteca.php
+++ Biblioteca/biblioteca.php
@@ -20,11 +20,15 @@ class Biblioteca{
     
 }
 
+
+
 $biblio=new Biblioteca();
-$ob1=new Libro('virtual','ISBN 856-96700-0-9','Gabo','100 años de soledad',1978,'Buen estado','EDITORIAL Planeta','200','20');
+$ob1=new Libro('virtual','ISBN 856-96700-0-9','Gabo','100 años de soledad','Buen estado','EDITORIAL Planeta','200','20');
+$ob2=new Revista('Papel estucado','ISSN 0028-0836','Pepe Perez','nature','Buen estado','Cientifico','03-09-2021','EEUU','19');
 
 $biblio->adicionar($ob);
 $biblio->adicionar($ob1);
+$biblio->adicionar($ob2);
 
 $biblio->verMateriales();
 
