7e3a24c0176388b2cf259a4f49b0e1333ecea8c5
JuanPablo122 <jpdoncel0@misena.edu.co>
2021-09-20 19:58:45 -0500

    v1 portafolio

---
+++ portafolio/vista/inicio.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../estilos/estilo.css">
+    <title>Portafolio</title>
+</head>
+<body>
+    <header class="header">        
+        <div class="container">
+            <div class="btn-menu">
+                <label for="btn-menu">☰</label>
+            </div>
+            <div class="texto">
+                <h2>Menú</h2>
+            </div>
+        </div>
+    </header>            
+    
+<input type="checkbox" id="btn-menu">
+<div class="container-menu">
+	<div class="cont-menu">
+		<nav>
+			<a href="inicio.php">Inicio</a>
+			<a href="#">Contenedores</a>
+			<a href="#">Crud</a>
+		</nav>
+		<label for="btn-menu">✖️</label>
+	</div>
+</div>
+<div class="prinseccion">
+    <img src="../imagenes/principal.jpg" alt="Imagen Principal">
+</div>
+
+
+</body>
+</html>
